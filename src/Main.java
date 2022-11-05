638d7ce36d511c2d75ce95117078c4caf7d6efca
Alexander <kuhar@mail.ru>
2022-11-05 18:26:35 +0300

    Выполнил дз "Таблица умножения"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private final Set<String> tasks = new HashSet<>(15);
+
+    public Main() {
+        while (tasks.size() < 16) {
+            String s = String.format("%d * %d", (int) (Math.random() * 8 + 2),
+                    (int) (Math.random() * 8 + 2));
+            if (tasks.contains(new StringBuilder(s).reverse().toString())) {
+                continue;
+            } else {
+                tasks.add(s);
+            }
+        }
     }
+
+    public void printTasks() {
+        for (String task : tasks) {
+            System.out.println(task);
+        }
+    }
+
+    public static void main(String[] args) {
+        Main st = new Main();
+        st.printTasks();
+    }
+
 }
